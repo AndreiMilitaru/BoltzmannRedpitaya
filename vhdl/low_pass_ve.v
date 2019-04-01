bb74fa55b2760d0c799d126b0552ed9b66674850
Andrei Militaru <andreimi@ethz.ch>
2019-04-01 09:44:23 +0200

    almost finished module

--- vhdl/low_pass_ve.v
+++ vhdl/low_pass_ve.v
@@ -9,6 +9,7 @@ module low_pass_ve #(
     parameter WIDTH = 16
 )(
     input wire clk_i,
+    input wire rst_ni,
     input wire [WIDTH-1:0] data_i,
     input wire [WIDTH-1:0] cutoff_i,
     
@@ -16,7 +17,22 @@ module low_pass_ve #(
     );
     
     reg [WIDTH-1:0] data_d, data_q;
+    reg [WIDTH-1:0] out_d, out_q;
+
     assign out_o = data_q;
-    
-    
+    assign data_i = data_d;
+
+    always @(posedge clk_i, negedge rst_ni) begin
+        if (~rst_ni) begin
+            out_q <= 0;
+            data_q <= 0;
+        end else begin
+            out_q <= out_d;
+            data_q <= data_d;
+        end
+    end
+
+    always @(*) begin
+        data
+
 endmodule
